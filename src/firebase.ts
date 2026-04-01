f0bc8c550d21c4898a781605792a297b45d1f9c7
SachinManral <sachinmanral2431@gmail.com>
2026-04-01 22:10:20 +0530

    Secure Firebase config using env variables

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,23 +1,23 @@
 // src/firebase.ts
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import { initializeAuth } from 'firebase/auth';
+import { getReactNativePersistence } from 'firebase/react-native-auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAIUWDdtKjktGUWkN4XKrzq6umVdxAbmjI",
-  authDomain: "nagarni.firebaseapp.com",
-  projectId: "nagarni",
-  storageBucket: "nagarni.appspot.com",
-  messagingSenderId: "793021981622",
-  appId: "1:793021981622:web:b4e640ff9185b1a543d717",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID!,
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
-
 export const storage = getStorage(app);
 
 export const auth = initializeAuth(app, {
